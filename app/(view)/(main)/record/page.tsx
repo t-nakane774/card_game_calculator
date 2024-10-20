fa7d33425fa5d79f74ff9f98914c34ace6da25af
中根宅望 <tnakane028@gmail.com>
2024-10-20 23:32:40 +0900

    record画面の作成した内容をコメントアウトでメモ

--- app/(view)/(main)/record/page.tsx
+++ app/(view)/(main)/record/page.tsx
@@ -1,5 +1,20 @@
 "use client"
 
+/**
+ * TODO
+ * デッキ種類タグを登録
+ * 自分が使っているデッキを選択
+ * 対戦相手が使ったデッキを選択
+ * 勝敗を入力
+ * 対戦内容を入力 2-1等
+ * db に対戦結果を保存
+ *
+ * デッキを選択した時に
+ * 全体勝率を表示
+ * 対戦相手デッキ種別毎の勝率を表示(円グラフを表示する？)
+ * 
+ * @returns 
+ */
 export default function Chart() {
   return (
     <main className='flex flex-col items-center p-24'>
